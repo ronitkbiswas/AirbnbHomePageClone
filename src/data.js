c36877b74e2614669aff1f8b219ac97ab3e30ec1
Ronit <ronitkbiswas@gmail.com>
2026-04-17 19:30:26 +0530

    final build data.js

--- src/data.js
+++ src/data.js
@@ -47,4 +47,6 @@ const generateListingsForCategory = (categoryId, categoryName, count = 25) => {
   });
 };
 
+// final export data set
+
 export const LISTINGS = CATEGORIES.flatMap(cat => generateListingsForCategory(cat.id, cat.name, 25));
